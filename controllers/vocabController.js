9f8aee1150a3ddefec1dfb910e3d01a9539e28e0
Priyanka6276 <priyankachanbrasegaran@gmail.com>
2022-12-26 21:04:16 -0500

    finished vocab controller

--- controllers/vocabController.js
+++ controllers/vocabController.js
@@ -2,3 +2,118 @@ const express = require("express")
 const router = express.Router()
 const Vocab = require("../models/vocab")
 
+//Seed Route
+router.get("/dashboard/seed", (req,res) => {
+    Vocab.create([
+        {
+            language: "개",
+            english: "dog",
+            category: "animal"
+
+        },
+        {
+            language: "버스",
+            english: "bus",
+            category: "transportation"
+        },
+        {
+            language: "집",
+            english: "house",
+            category: "home"
+        }
+    ], (err,data) => {
+        res.redirect("/vocab")
+    })
+})
+
+//Index
+router.get("/vocab", (req,res) => {
+    Vocab.find({}, (error, allVocabs) => {
+        if(!error){
+            res
+            .status(200)
+            .render("pages/VocabListPage", {
+                vocabs: allVocabs
+            })
+        } else {
+            res
+            .status(400)
+            .send(error)
+        }
+    })
+})
+
+//New 
+router.get("/vocab", (req,res) => {
+    res
+    .render("pages/VocabListPage")
+})
+
+//Delete
+router.delete("/vocab", (req,res) => {
+    Vocab.findByIdAndDelete(req.params.id, (err,data) => {
+        res.redirect("/vocab")
+    })
+})
+
+//Update
+router.put("/vocab", (req,res) => {
+    Vocab.findByIdAndUpdate(req.params.id, req.body, (err, updatedVocab) => {
+        if(!err){
+            res
+            .status(200)
+        } else {
+            res
+            .status(400)
+            .send(err)
+        }
+    })
+})
+
+//Create
+router.post("/vocab", (req,res) => {
+    Vocab.create(req.body, (error, createdVocab) => {
+        if(!error) {
+            res
+            .status(200)
+        } else {
+            res
+            .status(400)
+            .send(error)
+        }
+    })
+})
+
+//Edit
+router.get("/vocab", (req,res) => {
+    Vocab.findById(req.params.id, (err, foundVocab) => {
+        if(!err) {
+            res
+            .status(200)
+            .render("pages/VocabListPage", {vocab: foundVocab})
+        } else {
+            res
+            .status(400)
+            .send({mes: err.message})
+        }
+    })
+})
+
+//Show
+router.get("/vocab", (req,res) => {
+    Vocab.findById(req.params.id, (error, foundVocab) => {
+        if (!error) {
+            res
+            .status(200)
+            .render("pages/VocabListPage", {
+                vocab: foundVocab
+            })
+        } else {
+            res
+            .status(400)
+            .send(error)
+        }
+    })
+})
+
+module.export = router
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
 require("dotenv").config()
-
 require("./config/database")
+const app = express()
+const methodOverride = require('method-override')
+const vocabController = require("./controllers/vocabController")
 
-const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
+app.use(logger('dev'))
+app.use(express.json())
 
 
 
@@ -18,13 +18,25 @@ app.use(favicon(path.join(__dirname, "build", "favicon.ico")))
 app.use(express.static(path.join(__dirname, "build")))
 
 app.use(require("./config/checkToken"))
+app.use((req, res, next) => {
+    console.log("I'm running for all routes")
+    console.log("1.middleware")
+    next()
+})
+
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride("_method"))
+app.use(express.static('public'))
+
 
 
 app.use("/api/users", require("./routes/api/users"))
 
-app.get("/*", (req,res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"))
 })
+
+// app.use("/vocab", vocabController)
 
 
 
@@ -33,3 +45,4 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Express is running on port: ${PORT}`)
 })
+
